e3afd79768fcb2478f8db119e2afb6fe65bdcf60
Karel Spilka <karel.spil@gmail.com>
2019-08-01 22:20:09 +0300

    add full Task 1 in Lesson 1

--- OOP/script.js
+++ OOP/script.js
@@ -1,6 +1,11 @@
 //Lesson 1. Task 1.
 'use strict';
-
 var user = {
-    name : 'Cris'
+    name : 'Cris',
+    surname : 'Simons'
 };
+console.log(user.name);
+user.name = 'Brain';
+console.log(user.name);
+delete user.name
+console.log(user.name);
