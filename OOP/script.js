b1790d5dc206ee7349fbee4780b3494d464ab180
Karel Spilka <karel.spil@gmail.com>
2019-08-02 01:18:35 +0300

    Add task [2, 3, 4, 5, 6, 7] in Lesson 1(OOP)

--- OOP/script.js
+++ OOP/script.js
@@ -9,3 +9,142 @@ user.name = 'Brain';
 console.log(user.name);
 delete user.name
 console.log(user.name);
+
+//Lesson 1. Task 2.
+//Упращенный
+
+var car = {
+    model : 'Renault',
+    speed : 160,
+    run : function () {
+        console.log(car.model + ' едет со скоростью ' + car.speed);
+    },
+    stop : function () {
+        console.log(car.model + ' остановилась');
+    }
+};
+car.run();
+car.stop();
+//Стандартный
+
+var car = {};
+    car.model = 'Renault';
+    car.speed = 160;
+    car.run = function () {
+        console.log(car.model + ' едет со скоростью ' + car.speed);
+    };
+    car.stop = function () {
+        console.log(car.model + ' остановилась');
+    };
+car.run();
+car.stop();
+// Синтаксис массивов
+
+var car = {};
+    car['model'] = 'Renault';
+    car['speed'] = 160;
+    car['run'] = function () {
+        console.log(car.model + ' едет со скоростью ' + car.speed);
+    };
+    car['stop'] = function () {
+        console.log(car.model + ' остановилась');
+    };
+car['run']();
+car['stop']();
+
+//Lesson 1. Task 3.
+var tv = {
+    currentChannel : 1,
+    nextChannel : function(){
+        tv.currentChannel++;
+    },
+    previousChannel : function(){
+        tv.currentChannel--;
+    },
+    setChannel : function(number){
+        tv.currentChannel = number;
+    }
+};
+console.log(tv.currentChannel); 
+tv.setChannel(3);
+console.log(tv.currentChannel);
+
+//Lesson 1. Task 4.
+var mp3 = {
+    brand : 'Apple',
+    model : 'iPod',
+    codeName: 'Nano',
+    memory : 16,
+    haveDisplay: true,
+    battery : 600,
+    state : 'off',
+    powerOn : function(){
+        mp3.state = 'on'
+    }
+};
+console.log(mp3.state);
+mp3.powerOn();
+console.log(mp3.state);
+
+//Lesson 1. Task 5.
+var peoples = {
+    developers : [
+        {
+            name : 'John',
+            age : 28,
+            expirience : 1,
+            languages : ['Java', 'JavaScript', 'SQL']
+        }, {
+            name : 'Bill',
+            age : 30,
+            expirience : 5,
+            languages : ['HTML', 'CSS', 'JavaScript']
+        }, {
+            name : 'Mike',
+            age : 32,
+            expirience : 4,
+            languages : ['Phyton', 'SQL']
+        }
+    ]
+};
+console.log(peoples.developers[1].name);
+
+//Lesson 1. Task 6. !!!!!!!!!!!!!!!!!!!!
+function isEmpty(obj){
+    for (var key in obj){
+        if (key !== undefined){
+            console.log('false');
+        } else if (key == undefined) {
+            console.log('true');
+        };
+    };
+};
+
+var testPoint = {
+    name: 1
+}; 
+
+isEmpty(testPoint);
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+//Lesson 1. Task 7.
+// var salaries = {
+//     John : 100,
+//     Bill : 300,
+//     Mike : 250
+// };
+// console.log(salaries.__proto__.constructor.assign.length);
+var salaries = {
+    John : 400,
+    Cris : 150,
+    Brain : 600,
+    Bill : 300,
+    Mike : 50
+};
+var temp = 0;
+var res;
+for (var key in salaries){
+    temp += salaries[key];
+    res = temp / (Object.keys(salaries).length)
+};
+console.log('Средняя зарплата = ' + res);
