edc1978d346a3ad0a797c48f632a7e6fe9e476a1
Karel Spilka <karel.spil@gmail.com>
2019-08-08 21:55:03 +0300

    Add Task 2, 3, 4, 5, 6 in Lesson 2

--- OOP/script.js
+++ OOP/script.js
@@ -149,6 +149,8 @@ console.log('Средняя зарплата = ' + res);
 //```````````````````Lesson 2````````````````````````````
 //Lesson 2. Task 1.
 var calculator = {
+    firstNumber : 0,
+    secondNumber : 0,
     read : function (){
         this.firstNumber = Number(prompt('Введите первое число', 0));
         this.secondNumber = Number(prompt('Введите второе число', 0));
@@ -163,3 +165,155 @@ var calculator = {
 calculator.read();
 console.log('Cумма sum() =', calculator.sum());
 console.log('Произведение mul() =', calculator.mul());
+
+//Lesson 2.  Task 2.
+function createMP3 (brand, model, codeName, memory, haveDisplay, battery) {
+    return {
+        brand : brand,
+        model : model,
+        codeName: codeName,
+        memory : memory,
+        haveDisplay: haveDisplay,
+        battery : battery,
+        state : 'off',
+        powerOn : function(){
+            this.state = 'on';
+            console.log('Your ' + this.brand + ':' + this.model + ' mp3 player power ' + this.state);
+        },
+        powerOff : function(){
+            this.state = 'off';
+            console.log('Your ' + this.brand + ':' + this.model + ' mp3 player power ' + this.state);
+        }
+    };
+};
+
+var sony = createMP3 ('Sony', 'viao', 'x560sc', 8, true, 430);
+sony.powerOn();
+sony.powerOff();
+
+// //Lesson 2. Task 3.
+// function maxSalaries (salObj) {
+//     var max = 0;
+//     for (var key in salObj){
+//         if (salObj[key] > max){
+//             max = salObj[key];
+//         } else{
+//         return max;
+//         };
+//     };
+//     // return Math.max(salObj[key]);
+// };
+// var maxS = maxSalaries(salaries);
+// console.log(maxS);
+// // console.log(maxS);
+// // console.log(maxS);
+
+//Lesson 2. Task 4.
+function Mp3 (brand, model, codeName, memory, haveDisplay, battery) {
+    this.brand = brand;
+    this.model = model;
+    this.codeName = codeName;
+    this. memory = memory;
+    this.haveDisplay = haveDisplay;
+    this. battery = battery;
+    this.state = 'off';
+    this.powerOn = function (){
+        this.state = 'on';
+        console.log('Your ' + this.brand + ':' + this.model + ' mp3 player power ' + this.state);
+    };
+    this.powerOff = function (){
+        this.state = 'off';
+        console.log('Your ' + this.brand + ':' + this.model + ' mp3 player power ' + this.state);
+    }
+};
+
+var apple = new Mp3('apple', 'iPod', 'Nano', 8, true, 400);
+console.dir(apple);
+console.log(apple.brand);
+console.log(apple.codeName);
+apple.powerOn();
+apple.powerOff();
+
+//Lesson 2. Task 5.
+function mul() {
+    var res = 1;
+    var count = 0;
+    for (var i = 0; i < arguments.length; i++){
+        if (typeof(arguments[i])==="number"){
+            res *=arguments[i];
+            count++;
+        }
+    }
+    if (count == 0) return 0;
+    return res;
+};
+console.log(mul(1, 'str', 2, 3, true)); //6
+console.log(mul(null, 'str', false, true)); //0
+
+//Lesson 2. Task 6.
+var country = {
+    name : 'Ukraine',
+    language : 'ukrainian',
+    capital : {
+        name : 'Kiev',
+        population : 2907817,
+        area : 847.66
+    }
+};
+function format(beginMsg, endMsg) {
+    console.log(beginMsg + this.name + endMsg);
+};
+format.call(country, '"<', '>"');
+format.apply(country, ['"[', ']"']);
+format.call(country.capital, '""', '""');
+format.apply(country.capital, ['"', '"']);
+
+//```````````````````````````````````````````````````````
+//```````````````````Lesson 3````````````````````````````
+//Lesson 3. Task 1.
+//Метод bind
+var user = {
+    name: "Tom"
+};
+function format(beginMsg, endMsg) {
+    console.log(beginMsg + this.name + endMsg);
+}
+var tomFormat = format.bind(user);
+ tomFormat("<<<", ">>>"); // "<<<Tom>>>"
+ //Анонимную функцию обвертку
+var user = {
+    name: "Tom",
+    useFormat :  format
+};
+function format (beginMsg, endMsg) {
+    console.log(beginMsg + this.name + endMsg);
+};
+var tomFormat = function(){
+    user.useFormat("<<<", ">>>")
+};
+ tomFormat(); // "<<<Tom>>>"
+
+ //Lesson 3. Task 2.
+function mul(a, b) {
+    return a * b;
+}
+var doubleMul = mul.bind(null, 2);// Ваш код
+var qudraMul = mul.bind(null, 4);// Ваш код
+console.log(doubleMul(5)); // 10
+console.log(qudraMul(5)); // 20
+
+ //Lesson 3. Task 3.
+function bind(func, context) {
+    return function() {
+        return func.call(context, arguments)
+    };
+};
+function func() {
+    console.log(this.name + " - "+ this.age);
+}
+var user = {
+    name: "Tom",
+    age: 20
+};
+var f = bind(func, user);
+ f(); // "Tom – 20"
