4509f020ab0d41c9e75138f6afff169625be9892
Karel Spilka <karel.spil@gmail.com>
2019-08-19 18:40:55 +0300

    add task 1 in lesson 6

---
+++ OOP/script_lesson6.js
@@ -0,0 +1,20 @@
+//Lesson 6. Task 1.
+var square = {
+    x1 : 2,
+    y1 : 4,
+    x2 : 10,
+    y2 : 3,
+    toString : function () {
+        return 'Это прямоугольник!';
+    },
+    valueOf : function () {
+        var a;
+        var b;
+        a = this.y1 - this.y2;
+        b = this.x2 - this.x1;
+        return (2 * (a + b));
+
+    }
+};
+console.log(String(square));
+console.log(Number(square));
