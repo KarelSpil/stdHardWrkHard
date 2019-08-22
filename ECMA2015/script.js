70b9c2aaefaf6fa265aa7efad6ff1936369790a1
Karel Spilka <karel.spil@gmail.com>
2019-08-23 00:22:43 +0300

    Add task 1,2,3 in Lesson 2(ECMA2015)

--- ECMA2015/script.js
+++ ECMA2015/script.js
@@ -31,28 +31,101 @@ console.log(name4);
 console.log(age4);
 
 //Lesson 1. Task 4.
-/* Напишите функц mul(), которая принемает любое колво парам разного типа и возвращает произвед параметров типа Number.
-Если парам типа Number отсутствуют, возвращает число 0.
-Нельзя использовать arguments, но в функцию можно добавить один парам.
-разобрать эту ерунду с Максом */
-
 function mul (...all) {
     let newArr = all.filter(function(number){
-    	if (typeof number == "number"){
-    		return number;		
-    	};
+        if (typeof number == "number"){
+            return number;		
+        };
     });
     let res = 1;
     if (typeof newArr[0] == "number"){
-    	function mult (value) {
-    	return res *= value;
-    	};
-    	newArr.forEach(mult);
-    	return res;
+        function mult (value) {
+            return res *= value;
+        };
+        newArr.forEach(mult);
+        return res;
     } else if (typeof newArr[0] != "number"){
-    	return res = 0;
+        return res = 0;
     };
 };
 console.log(mul(1, 'str', 2, 3, true));
 console.log(mul(null, 'str', false, true));
+
+//Lesson 2. Task 1.
+let tv = {
+    currentChannel : 0,
+    nextChannel () {
+        this.currentChannel = ++this.currentChannel;
+        console.log(this.currentChannel);
+    },
+    previousChannel () {
+        this.currentChannel = --this.currentChannel;
+        console.log(this.currentChannel);
+    },
+    setChannel (number) {
+        this.currentChannel = number;
+        console.log(this.currentChannel);
+    }
+};
+let t = tv;
+t.nextChannel();
+t.setChannel(78);
+
+//Lesson 2. Task 2.
+let mp3 = {
+    _barand : 'Apple',
+    _status : false,
+    _memory : 16,
+    getStatus () {
+        return this._status;
+    },
+    on () {
+        this._status = true;
+    },
+    off () {
+        this._status = false;
+    }
+};
+let m = mp3;
+console.dir(m);
+console.log(m.getStatus());
+m.on();
+console.log(m.getStatus());
+
+//Lesson 2. Task 3.
+class Tv {
+    constructor(){
+    this.currentChannel = 0;
+    }
+    nextChannel () {
+        this.currentChannel = ++this.currentChannel;
+    }
+    previousChannel () {
+        this.currentChannel = --this.currentChannel;
+    }
+    setChannel (number) {
+        this.currentChannel = number;
+    }
+};
+const ti = new Tv();
+
+class Mp3 {
+    constructor (name){
+    this._barand = name;
+    this._status = false;
+    this._memory = 16;
+    }
+    getStatus () {
+        return this._status;
+    }
+    on () {
+        this._status = true;
+    }
+    off () {
+        this._status = false;
+    }
+};
+const mp = new Mp3("Sony");
+
+
 
